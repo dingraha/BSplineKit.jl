ee650bb0273296e0b3a9b116bc99b8e24590b146
Juan Ignacio Polanco <jipolanc@gmail.com>
2022-05-19 08:21:28 +0000

    build based on 16c689d

--- versions.js
+++ versions.js
@@ -1,5 +1,10 @@
 var DOC_VERSIONS = [
   "stable",
+  "v0.11",
+  "v0.10",
+  "v0.9",
+  "v0.8",
+  "v0.7",
   "v0.6",
   "v0.5",
   "v0.4",
@@ -8,3 +13,5 @@ var DOC_VERSIONS = [
   "v0.1",
   "dev",
 ];
+var DOCUMENTER_NEWEST = "v0.11.2";
+var DOCUMENTER_STABLE = "stable";
